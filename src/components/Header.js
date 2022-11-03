7e14615ba2608535067ebab9222cd42bb1a3c6d9
Varsha Rawat <varsha.rawat690@gmail.com>
2022-11-03 16:23:58 +0530

    file added

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Form';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div>
+        <h1>Flicker</h1>
+        <Form/>
+    </div>
+  );
+};
+
+export default Header;
